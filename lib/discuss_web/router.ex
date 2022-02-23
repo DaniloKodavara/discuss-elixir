62cf6c8a50b1438f8a49426be1369cba2b299821
Danilo Kodavara <danilo.kodavara@hotmail.com>
2022-02-23 20:41:51 -0300

    Iniciando a criacao dos modelos de topicos.

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -4,7 +4,7 @@ defmodule DiscussWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_live_flashsoli
+    plug :fetch_live_flash
     plug :put_root_layout, {DiscussWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
