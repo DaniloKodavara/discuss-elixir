2e945b00f680feb3135dc168251aeb8f853b0dc0
Danilo Kodavara <danilo.kodavara@hotmail.com>
2022-02-23 23:03:02 -0300

    Finalizando html para criacao do topico, e iniciando a criacao de endpoint para salvar topico.

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,8 +1,12 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
-  def new conn, params do
+  alias DiscussWeb.Topic
 
+  def new conn, params do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
   end
 
 end
