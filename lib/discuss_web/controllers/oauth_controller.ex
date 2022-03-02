890b6aebf5b4228af01839e825b8a31a610bda2c
Danilo Kodavara <danilo.kodavara@hotmail.com>
2022-03-02 20:28:58 -0300

    Criando logica de logout da sessao.

--- lib/discuss_web/controllers/oauth_controller.ex
+++ lib/discuss_web/controllers/oauth_controller.ex
@@ -5,7 +5,7 @@ defmodule DiscussWeb.AuthController do
 
   plug Ueberauth
 
-  def callback(%{assigns: %{ueberauth_auth: auth} } = conn, %{"provider" => provider} = params) do
+  def callback(%{assigns: %{ueberauth_auth: auth} } = conn, %{"provider" => provider}) do
     user_params = %{email: auth.info.email, provider: provider, token: auth.credentials.token}
 
     changeset = User.changeset(%User{}, user_params)
@@ -25,6 +25,12 @@ defmodule DiscussWeb.AuthController do
         |> put_flash(:error, "Error signing in")
         |> redirect(to: Routes.topic_path(conn, :index))
     end
+  end
+
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: Routes.topic_path(conn, :index))
   end
 
   defp insert_or_update_user(changeset) do
